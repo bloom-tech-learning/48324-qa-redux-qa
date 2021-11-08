543ad83cb455d0e5c18375ba06b3fa9871e79604
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-07 19:43:17 -0800

    demo - thunk, first setup thunk in index.js, then add a funtion in action return function

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,9 @@ export const ADD_TODO = "ADD_TODO";
 export const addTodo = (title) => {
     return { type: ADD_TODO, payload: title };
   };
+
+  export const addThreeTodos = (title) => {
+    return (dispatch => {
+
+    })
+  }
