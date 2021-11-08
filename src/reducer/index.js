fb118d7d303290feddf1a5d699d28c3357dc453d
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-07 19:20:39 -0800

    complete reducer ADD_TODO

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,4 @@
-
+import { ADD_TODO } from '../actions'
 export const initialState = {
     todos: [
         {title: "item 1", completed: false, id: 123},
@@ -11,12 +11,25 @@ export const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_TODO:
+            //1. I need to get the todo title
+            //2. return state with changed todos
+            //3. add on a todo with title as action.payload
+            //4. return todos with new todo
+        const newTodo = {
+            title: action.payload,
+            completed: false,
+            id: Date.now()
+        };
+
+        return {
+            ...state,
+            todos: [...state.todos, newTodo]
+          };
 
         default:
             return state;
-
-    }
-        
+    }        
 };
 
 export default reducer;
