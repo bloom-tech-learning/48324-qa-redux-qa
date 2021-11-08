b4384e01f6d9ae3ea002b50c66eb8a35eb8fa678
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-07 17:44:38 -0800

    write in App the scatter in to TodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> qa-324 </h1>
+      <h1> Todo </h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+
+      <form>
+        <input />
+        <button>Submit Todo</button>
+        <button>Submit Three</button>
+      </form>
     </div>
   );
 }
