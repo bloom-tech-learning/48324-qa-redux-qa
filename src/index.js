984fe377ce25dc8c718fda8d08a94fa0af40ad69
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-07 18:16:34 -0800

    setup redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import App from "./App";
 import React from 'react'
 import ReactDOM from "react-dom";
+
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 
@@ -7,12 +9,12 @@ import thunk from "redux-thunk";
 
 import reducer from "./reducer";
 
+const store = createStore(
+  reducer, 
+  applyMiddleware(thunk)
+  );
 
-import App from "./App";
-
-const store = createStore(reducer, applyMiddleware(thunk));
-
-
+console.log('store.getState(): ', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
