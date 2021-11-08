a03162f107795009c3126187dba6c3202809c754
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-07 19:34:55 -0800

    executevthe action in TodoForm, - props.dispatch(addTodo("this is a new title"));

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,8 +5,9 @@ import { connect } from "react-redux"
 const TodoForm = (props) => {
     const handleSubmit =(e) => {
         e.preventDefault();
-        console.log('dispatch and todo action');
-        console.log('props: ', props);    
+        // console.log('dispatch and todo action');
+        // console.log('props: ', props); 
+        props.dispatch(addTodo("this is a new title"));   
     }
     return (
       <form>
