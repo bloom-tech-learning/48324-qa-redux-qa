17b52999fa2b0a2d8d4d5cadbaf0990d1e57c35a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-07 18:11:41 -0800

    hookup reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,12 +1,21 @@
 
 export const initialState = {
+    todos: [
+        {title: "item 1", complete: false, id: 123},
+        {title: "item 2", complete: false, id: 456},
+        {title: "item 3", complete: false, id: 789}
+    ]
 
 };
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
 
-    
-        return state;
-}
+        default:
+            return state;
+
+    }
+        
+};
 
 export default reducer;
